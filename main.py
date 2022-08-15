d6d80722c2afbbbf5033c3cd400b2012f84f3bde
Ben Tettmar <bentettmar@yahoo.com>
2022-08-15 09:56:38 +0100

    Switched to using httpx for faster requests

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 import colorama
 import time
 
-choice = input("Do you want to check for characters or words? (c/w): ")
+http = httpx.Client()
 
 if (choice == "c"):
     i = int(input("How much char names you want to check: "))
 delay = float(input("How much seconds you want to wait between requests: "))
 
-f = open("names.txt", "a")
+    resp = http.get(f"https://github.com/{name}")
 
 def checkchar():
     name = ""
@@ -27,9 +27,7 @@ def checkchar():
 
 def checkword():
     name = ""
-    name = requests.get("https://random-word-api.herokuapp.com/word").text.strip('"[]')
-    r = requests.get(f"https://github.com/{name}")
-    if (r.status_code == 404):
+    name = http.get("https://random-word-api.herokuapp.com/word").json()[0]
         print(colorama.Fore.GREEN + f"{name} is available!")
         f.write(f"{name}\n")
     elif (r.status_code == 200):
